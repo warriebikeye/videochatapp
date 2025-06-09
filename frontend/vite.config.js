ae07360ce43666d84502a497a8eed4548f261078
Ebikeye <ware_ebi@yahoo.com>
2025-06-09 15:25:15 +0100

    updatedviteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose the server to all interfaces
+    port: 10000,      // Set the port Render uses
+    strictPort: true, // Ensures Vite fails if the port is taken
+  },
 })
